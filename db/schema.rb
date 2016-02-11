3f6b9b3513c80dc6f9b379625a6783f2c88bf873
Carl Eqladios <ceqladios@gmail.com>
2016-02-11 02:47:36 +0200

    Create Admin boolean in users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210231116) do
+ActiveRecord::Schema.define(version: 20160211004543) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160210231116) do
     t.string   "name"
     t.string   "home"
     t.string   "work"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
