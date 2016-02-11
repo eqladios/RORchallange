aafc151c2bb382ab22a02b1c2325bfa033349943
Carl Eqladios <ceqladios@gmail.com>
2016-02-11 19:28:19 +0200

    Add edit and view for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def index
     @users = User.all
   end
@@ -7,5 +6,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
 end
