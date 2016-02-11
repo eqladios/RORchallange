174a2f92b746f506fa502930cbf657846febde67
Carl Eqladios <ceqladios@gmail.com>
2016-02-11 20:15:22 +0200

    Migrate Trips Table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211004543) do
+ActiveRecord::Schema.define(version: 20160211181138) do
+
+  create_table "trips", force: :cascade do |t|
+    t.string   "time"
+    t.string   "date"
+    t.string   "source"
+    t.string   "destination"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
