ef2a1029454667382adfe92c809c7c998837c4d6
Carl Eqladios <ceqladios@gmail.com>
2016-02-12 16:38:41 +0200

    Make Name,Home,Work Required

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         validates_presence_of :name
+         validates_presence_of :home
+         validates_presence_of :work
 end
