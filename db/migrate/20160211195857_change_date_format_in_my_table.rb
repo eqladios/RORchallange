2b642344596d97608c917ace7a6dc6c72892573a
Carl Eqladios <ceqladios@gmail.com>
2016-02-12 23:00:26 +0200

    Update Migration for Heroku

--- db/migrate/20160211195857_change_date_format_in_my_table.rb
+++ db/migrate/20160211195857_change_date_format_in_my_table.rb
@@ -1,6 +1,6 @@
 class ChangeDateFormatInMyTable < ActiveRecord::Migration
   def change
-    change_column :trips, :date,  :date
-    change_column :trips, :time,  :time
+    change_column :trips, :date, 'date USING CAST(date AS date)'
+    change_column :trips, :time, 'time USING CAST(time AS time)'
   end
 end
