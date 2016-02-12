761efe42b1c79874ff208fdf3d94b4cbe4bc8c76
Carl Eqladios <ceqladios@gmail.com>
2016-02-12 18:39:45 +0200

    Changed Trips Views design

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,7 @@
 class Trip < ActiveRecord::Base
   belongs_to :user
+  validates_presence_of :time
+  validates_presence_of :date
+  validates_presence_of :source
+  validates_presence_of :destination
 end
