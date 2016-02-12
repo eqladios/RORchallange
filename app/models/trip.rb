b817c1c4b3fe06d9bc4bd65a7ef84f8032c90c62
Carl Eqladios <ceqladios@gmail.com>
2016-02-12 19:03:12 +0200

    Create Membership relationship between User and Trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,7 @@
 class Trip < ActiveRecord::Base
   belongs_to :user
+  has_many :memberships, :dependent => :destroy
+  has_many :users, :through => :memberships
   validates_presence_of :time
   validates_presence_of :date
   validates_presence_of :source
