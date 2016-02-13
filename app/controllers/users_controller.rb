b7d894a3d1561de2c6b300a9afa9f65c6f3e5db4
Carl Eqladios <ceqladios@gmail.com>
2016-02-13 08:00:29 +0200

    remove unused code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,5 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_create :generate_authentication_token
-
-  def generate_authentication_token
-    loop do
-      self.authentication_token = SecureRandom.base64(64)
-      break unless User.find_by(authentication_token: authentication_token)
-    end
-  end
-  
   def index
     @users = User.all
   end
